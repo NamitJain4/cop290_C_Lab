dc0a1d69284af0cdf167090e41c15febd54e4050
Rishabh Narayanan <160815424+rishnars95@users.noreply.github.com>
2025-02-23 00:00:54 +0530

    Update_1 main.c

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 /* Scanner + Parser */
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int print_sheet = 1;
+float exec_time = 0.0;
+char *exec_status = "ok";
+char *err = "unrecognized cmd";
+char *cmd = "\0";
 
 int main()
 {
-  return 0
+    while (strcmp(cmd, "q")) {
+        if (print_sheet) {
+        printf("[%.1f] (%s) > ", exec_time, exec_status);
+        scanf("%s", cmd);
+        }
+    }
+}
+
+void scan()
+{
+    return;
 }
