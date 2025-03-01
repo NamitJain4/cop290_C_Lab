fc4504e42639f108db5c35c3d90cd5ab8aeb81d8
Namit Jain <64017532+NamitJain4@users.noreply.github.com>
2025-03-02 03:27:47 +0530

    Update operations_implementation.c

--- operations_implementation.c
+++ operations_implementation.c
@@ -9,22 +9,8 @@
 #define sleep(x) Sleep(1000*(x))
 
 
-
-// even though the logic is complete and bug free (hopefully)
-// ill making some edits in the file here and there, to make it faster and more memory efficient
-// ~ namit
-
-
 /*
-ALL OPERATIONS IMPLEMENTED
-TOPOLOGICAL SORT IMPLEMENTED TO OPTIMIZE THE TIME COMPLEXITY
-MADE 2 TESTCASES (DIDN'T TEST ALL OPERATIONS)
-
-
-A1 = 2 - B1
-A1 = B1 - 2
-
-                      opcode
+                    opcode
 A1 = B1 - B2        -
 A1 = B1 + B2        +
 A1 = B1 * B2        *
@@ -46,29 +32,37 @@ struct Node {
     int dCount;                   // number of nodes the current node is dependent upon
     char opcode;                  // opcode for dep_upon
     int constant;                 // constant involved, for eg. 1 in the case of A1 = B1 + 1 (toh A1 ka constant is 1)
-    int old_val;                  // old value, to update the dependencies based on that
     int visited;                  // for topsort
+    int is_err;                   // to check if the node is an error node
 };
 
 void updateNode(struct Node *node, struct Node **dep_upon, int dCount, char opcode, int new_constant);
 int calcValue(struct Node *node);
 void printNodeDetails(struct Node *node);
-void sort(struct Node **order[], int *length, struct Node *node);
+void sort(struct Node **order[], int *length, struct Node *node, int *cycle);
 void set_visited_to_zero(struct Node *order[], int length);
+void set_node_to_error(struct Node *node);
 
 
 
-// taking a pointer to a pointer to a node array
+// taking a pointer to a (pointer to a node) array
 // (because realloc may change the address of the array of pointers entirely, and that wouldn't be reflected outside the function)
-void sort(struct Node **order[], int *length, struct Node *node) {
+void sort(struct Node **order[], int *length, struct Node *node, int *cycle) {
+    if (node->visited == 1) {
+        *cycle = 1;
+        return;
+    }
+    if (node->visited == 2) {
+        return;
+    }
     node->visited = 1;
     for (int i = 0; i < node->depCount; i++) {
-        if (!(node->dependencies[i]->visited)) {
-            sort(order, length, node->dependencies[i]);
+        if (*cycle) {
+            return;
         }
+        sort(order, length, node->dependencies[i], cycle);
     }
-    // append the node to the order array, by increasing its size too
-    // printf("%s %d\n", node->name, *length);
+    node->visited = 2;
     *order = realloc(*order, (*length + 1) * sizeof(struct Node*));
     (*order)[(*length)++] = node;
 }
@@ -79,10 +73,41 @@ void set_visited_to_zero(struct Node *order[], int length) {
     }
 }
 
+// node => pointer to the node that you want to change,
+// dep_upon => array of pointers to the nodes that the current node is going to be dependent upon,
+// dCount => number of nodes in the dep_upon array,
+// opcode => opcode,
+// new_constant => new constant
 void updateNode(struct Node *node, struct Node **dep_upon, int dCount, char opcode, int new_constant) {
     // A1 = 2 - B1, expecting dep_upon = [NULL, B1], dCount = 2
-    // A1 = 2, dCount = 0 and dep_upon = []
+    // A1 = 2, dCount = 0 and dep_upon = [], new_constant = 2, opcode = +
     // A1 = B1, new_constant should be 0, opcode should be +
+    // A1 = 2 / B1, new_constant = 2, opcode = /, dep_upon = [NULL, B1], dCount = 2
+    // A1 = B1 / 2, new_constant = 2, opcode = /, dep_upon = [B1], dCount = 1
+    // A1 = B1 / B2, new_constant = 0, opcode = /, dep_upon = [B1, B2], dCount = 2
+    // A1 = max(B1:C2), dep_upon = [C2, B1, B2, C1] (any order), dCount = 4
+    // A1 = 2 * A1, dep_upon = [A1], dCount = 1
+    // A1 = 2 * 3, dep_upon = [], dCount = 0, new_constant = 6
+    // note: in the above example, you must calculate 2*3 and store it in new_constant
+    // for sleep ask ayush
+
+
+    struct Node **order = malloc(sizeof(struct Node *));
+    int length = 0;
+    int cycle = 0;
+    for (int i = 0; i < dCount; i++) {
+        dep_upon[i]->visited = 1;
+    }
+    sort(&order, &length, node, &cycle);
+    set_visited_to_zero(order, length);
+    for (int i = 0; i < dCount; i++) {
+        dep_upon[i]->visited = 0;
+    }
+    if (cycle) {
+        printf("Error: Cycle detected. Rejected the operation.\n");
+        free(order);
+        return;
+    }
 
     // traverse through the dep_upon array
     for (int i = 0; i < node->dCount; i++) {
@@ -114,7 +139,7 @@ void updateNode(struct Node *node, struct Node **dep_upon, int dCount, char opco
     node->constant = new_constant;
 
     // change value of the current node
-    node->value = calcValue(node);
+    // node->value = calcValue(node);
 
     // traverse through the nodes in the dep_upon array
     for (int i = 0; i < node->dCount; i++) {
@@ -126,28 +151,43 @@ void updateNode(struct Node *node, struct Node **dep_upon, int dCount, char opco
 
     // debugging
     //printNodeDetails(node);
-
-    struct Node **order = malloc(sizeof(struct Node *));
-    int length = 0;
-    sort(&order, &length, node);
-    set_visited_to_zero(order, length);
     /*for (int i = 0; i < length; i++) {
-        printf("%s ", order[i]->name);
+        printf("%s %d  ", order[i]->name, order[i]->value);
     }
     printf("\n \t%d \n", length);*/
     
     for (int i = length - 1; i >= 0; i--) {
         order[i]->value = calcValue(order[i]);
     }
-    free(order);
+}
+
+void set_node_to_error(struct Node *node) {
+    node->is_err = 1;
+    free(node->dependencies);
+    node->dependencies = NULL;
+    node->depCount = 0;
 }
 
 int calcValue(struct Node *node) {
+    if (node->is_err) {
+        free(node->dependencies);
+        node->dependencies = NULL;
+        node->depCount = 0;        
+    }
+    node->is_err = 0;
+
     if (node->dCount == 0) {
         if (node->opcode == 'l'){
             sleep(node->constant);
         }
         return node->constant; // No dependencies, value equals constant
+    }
+
+    for (int i = 0; i < node->dCount; i++) {
+        if (node->dependent_upon[i] != NULL && node->dependent_upon[i]->is_err) {
+            set_node_to_error(node);
+            return 0;
+        }
     }
 
     if (node->opcode == '+') {
@@ -176,14 +216,16 @@ int calcValue(struct Node *node) {
                 }
                 return node->constant / node->dependent_upon[1]->value;
             } else {
-                printf("Error: Division by zero\n");
+                printf("Error: Division by zero occured while calculating node: %s\n", node->name);
+                set_node_to_error(node);
                 return 0;
             }
         }
         if (node->constant != 0) {
             return node->dependent_upon[0]->value / node->constant;
         } else {
-            printf("Error: Division by zero\n");
+            printf("Error: Division by zero occured while calculating node: %s\n", node->name);
+            set_node_to_error(node);
             return 0;
         }
     } else if (node->opcode == 'M') {
@@ -238,6 +280,8 @@ int calcValue(struct Node *node) {
 void printNodeDetails(struct Node *node) {
     printf("Node %s:\n", node->name);
     printf("  Value: %d\n", node->value);
+    printf("  Is Error: %d\n", node->is_err);
+    printf("  Is Visited: %d\n", node->visited);
     printf("  Dependencies: ");
     for (int i = 0; i < node->depCount; i++) {
         printf("%s ", node->dependencies[i]->name);
@@ -262,76 +306,45 @@ int main() {
     16 12 4 12 16
     */
 
-    // Create nodes on the heap
-    struct Node *A1 = malloc(sizeof(struct Node));
-    struct Node *B1 = malloc(sizeof(struct Node));
-    struct Node *B2 = malloc(sizeof(struct Node));
-    struct Node *C1 = malloc(sizeof(struct Node));
-    struct Node *D1 = malloc(sizeof(struct Node));
-
-    // Initialize nodes
-    *A1 = (struct Node){"A1", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
-    *B1 = (struct Node){"B1", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
-    *B2 = (struct Node){"B2", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
-    *C1 = (struct Node){"C1", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
-    *D1 = (struct Node){"D1", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
+    // Create nodes
+    struct Node A1_1 = {"A1", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
+    struct Node B1_1 = {"B1", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
+    struct Node B2_1 = {"B2", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
+    struct Node C1_1 = {"C1", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
+    struct Node D1_1 = {"D1", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
 
     // Initialize values using updateNode
-    updateNode(B1, NULL, 0, '+', 3);
-    updateNode(B2, NULL, 0, '+', 4);
+    updateNode(&B1_1, NULL, 0, '+', 3);
+    updateNode(&B2_1, NULL, 0, '+', 4);
 
     // Set dependencies
-    struct Node* A1_deps[] = {B1, B2};
-    struct Node* C1_deps[] = {A1, B2};
-    struct Node* D1_deps[] = {A1, C1};
-    struct Node* C1_deps_2[] = {B2};
+    struct Node* A1_deps_1[] = {&B1_1, &B2_1};
+    struct Node* C1_deps_1[] = {&A1_1, &B2_1};
+    struct Node* D1_deps_1[] = {&A1_1, &C1_1};
+    struct Node* C1_deps_2_1[] = {&B2_1};
 
     // Update nodes
-    updateNode(A1, A1_deps, 2, '+', 0);
-    updateNode(C1, C1_deps, 2, '*', 0);
-    updateNode(D1, D1_deps, 2, 'M', 0);
-    updateNode(C1, C1_deps_2, 1, '+', 8);
-    updateNode(B1, NULL, 0, '+', 12);
+    updateNode(&A1_1, A1_deps_1, 2, '+', 0);
+    updateNode(&C1_1, C1_deps_1, 2, '*', 0);
+    updateNode(&D1_1, D1_deps_1, 2, 'M', 0);
+    updateNode(&C1_1, C1_deps_2_1, 1, '+', 8);
+    updateNode(&B1_1, NULL, 0, '+', 12);
     // 16 12 4 12 16
 
     // Print results
-    printNodeDetails(A1);
-    printNodeDetails(B1);
-    printNodeDetails(B2);
-    printNodeDetails(C1);
-    printNodeDetails(D1);
-
-    // Free allocated memory
-    free(A1->dependencies);
-    free(A1->dependent_upon);
-    free(A1);
-    free(B1->dependencies);
-    free(B1->dependent_upon);
-    free(B1);
-    free(B2->dependencies);
-    free(B2->dependent_upon);
-    free(B2);
-    free(C1->dependencies);
-    free(C1->dependent_upon);
-    free(C1);
-    free(D1->dependencies);
-    free(D1->dependent_upon);
-    free(D1);
+    printNodeDetails(&A1_1);
+    printNodeDetails(&B1_1);
+    printNodeDetails(&B2_1);
+    printNodeDetails(&C1_1);
+    printNodeDetails(&D1_1);
 
     printf("\n\n-------------------- TC - 2 --------------------\n");
 
-    struct Node *A1_ = malloc(sizeof(struct Node));
-    struct Node *B1_ = malloc(sizeof(struct Node));
-    struct Node *B2_ = malloc(sizeof(struct Node));
-    struct Node *B3_ = malloc(sizeof(struct Node));
-    struct Node *B4_ = malloc(sizeof(struct Node));
-
-    // Initialize nodes
-    *A1_ = (struct Node){"A1", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
-    *B1_ = (struct Node){"B1", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
-    *B2_ = (struct Node){"B2", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
-    *B3_ = (struct Node){"B3", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
-    *B4_ = (struct Node){"B4", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
+    struct Node A1_2 = {"A1", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
+    struct Node B1_2 = {"B1", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
+    struct Node B2_2 = {"B2", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
+    struct Node B3_2 = {"B3", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
+    struct Node B4_2 = {"B4", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
 
     /*
     A1 = 3
@@ -342,39 +355,105 @@ int main() {
     A1 = 4
     4 5 -6 10 -2
     */ 
-    updateNode(A1_, NULL, 0, '+', 3);
-    struct Node* B1_deps_[] = {A1_};
-    updateNode(B1_, B1_deps_, 1, '+', 1);
-    struct Node* B3_deps_[] = {B1_};
-    updateNode(B3_, B3_deps_, 1, '*', 2);
-    struct Node* B2_deps_[] = {A1_, B3_};
-    updateNode(B2_, B2_deps_, 2, '-', 0);
-    struct Node* B4_deps_[] = {A1_, B2_};
-    updateNode(B4_, B4_deps_, 2, '+', 0);
-    updateNode(A1_, NULL, 0, '+', 4);
+    updateNode(&A1_2, NULL, 0, '+', 3);
+    struct Node* B1_deps_2[] = {&A1_2};
+    updateNode(&B1_2, B1_deps_2, 1, '+', 1);
+    struct Node* B3_deps_2[] = {&B1_2};
+    updateNode(&B3_2, B3_deps_2, 1, '*', 2);
+    struct Node* B2_deps_2[] = {&A1_2, &B3_2};
+    updateNode(&B2_2, B2_deps_2, 2, '-', 0);
+    struct Node* B4_deps_2[] = {&A1_2, &B2_2};
+    updateNode(&B4_2, B4_deps_2, 2, '+', 0);
+    updateNode(&A1_2, NULL, 0, '+', 4);
 
-    printNodeDetails(A1_);
-    printNodeDetails(B1_);
-    printNodeDetails(B2_);
-    printNodeDetails(B3_);
-    printNodeDetails(B4_);
+    printNodeDetails(&A1_2);
+    printNodeDetails(&B1_2);
+    printNodeDetails(&B2_2);
+    printNodeDetails(&B3_2);
+    printNodeDetails(&B4_2);
 
-    // Free allocated memory
-    free(A1_->dependencies);
-    free(A1_->dependent_upon);
-    free(A1_);
-    free(B1_->dependencies);
-    free(B1_->dependent_upon);
-    free(B1_);
-    free(B2_->dependencies);
-    free(B2_->dependent_upon);
-    free(B2_);
-    free(B3_->dependencies);
-    free(B3_->dependent_upon);
-    free(B3_);
-    free(B4_->dependencies);
-    free(B4_->dependent_upon);
-    free(B4_);
+    printf("\n\n-------------------- TC - 3 --------------------\n");
+    /*
+    A1 = 5
+    B1 = 6
+    B2 = 0
+    C1 = A1 + 2
+    D1 = A1 * 3
+    E1 = C1 + A1
+    F1 = C1 - 1
+    A1 = B1 / B2
+    B2 = 2
+    */
+
+    // Create nodes
+    struct Node A1_3 = {"A1", 0, NULL, 0, NULL, 0, '/', 0, 0, 0};
+    struct Node B1_3 = {"B1", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
+    struct Node B2_3 = {"B2", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
+    struct Node C1_3 = {"C1", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
+    struct Node D1_3 = {"D1", 0, NULL, 0, NULL, 0, '*', 0, 0, 0};
+    struct Node E1_3 = {"E1", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
+    struct Node F1_3 = {"F1", 0, NULL, 0, NULL, 0, '-', 0, 0, 0};
+
+    // Initialize values using updateNode
+    updateNode(&A1_3, NULL, 0, '+', 5);
+    updateNode(&B1_3, NULL, 0, '+', 6);
+    updateNode(&B2_3, NULL, 0, '+', 0);
+
+    // Set dependencies
+    struct Node* A1_deps_3[] = {&B1_3, &B2_3};
+    struct Node* C1_deps_3[] = {&A1_3};
+    struct Node* D1_deps_3[] = {&A1_3};
+    struct Node* E1_deps_3[] = {&C1_3, &A1_3};
+    struct Node* F1_deps_3[] = {&C1_3};
+
+    // Update nodes
+    updateNode(&C1_3, C1_deps_3, 1, '+', 2);
+    updateNode(&D1_3, D1_deps_3, 1, '*', 3);
+    updateNode(&E1_3, E1_deps_3, 2, '+', 0);
+    updateNode(&F1_3, F1_deps_3, 1, '-', 1);
+    updateNode(&A1_3, A1_deps_3, 2, '/', 0);
+    updateNode(&B2_3, NULL, 0, '+', 2);
+
+    // Print results
+    printNodeDetails(&A1_3);
+    printNodeDetails(&B1_3);
+    printNodeDetails(&B2_3);
+    printNodeDetails(&C1_3);
+    printNodeDetails(&D1_3);
+    printNodeDetails(&E1_3);
+    printNodeDetails(&F1_3);
+
+    printf("\n\n-------------------- TC - 4 --------------------\n");
+    /*
+    A1 = 1
+    B1 = A1 + 2
+    C1 = B1 + 3
+    A1 = C1 - 5
+    */
+
+    // Create nodes
+    struct Node A1_4 = {"A1", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
+    struct Node B1_4 = {"B1", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
+    struct Node C1_4 = {"C1", 0, NULL, 0, NULL, 0, '+', 0, 0, 0};
+
+    // Initialize values using updateNode
+    updateNode(&A1_4, NULL, 0, '+', 1);
+
+    // Set dependencies
+    struct Node* B1_deps_4[] = {&A1_4};
+    struct Node* C1_deps_4[] = {&B1_4};
+    struct Node* A1_deps_4[] = {&C1_4};
+
+    // Update nodes
+    updateNode(&B1_4, B1_deps_4, 1, '+', 2);
+    updateNode(&C1_4, C1_deps_4, 1, '+', 3);
+    updateNode(&A1_4, A1_deps_4, 1, '-', 5);
+
+    // Print results
+    printNodeDetails(&A1_4);
+    printNodeDetails(&B1_4);
+    printNodeDetails(&C1_4);
+
 
     return 0;
 }
